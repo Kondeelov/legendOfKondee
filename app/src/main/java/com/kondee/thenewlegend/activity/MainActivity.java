07c6925bce53be1bccfa8a7e10c6519ee4e7b34a
Kondee <spirit_of_naruto@hotmail.com>
2016-09-21 18:18:07 +0700

    Update 21/09/2016, 18:18

--- app/src/main/java/com/kondee/thenewlegend/activity/MainActivity.java
+++ app/src/main/java/com/kondee/thenewlegend/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
